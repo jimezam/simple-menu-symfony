eb2aee9cd74fb6f3c86fd022e431243dcb35ded1
Jorge I. Meza <jimezam@gmail.com>
2025-01-06 21:53:01 -0500

    Added Restaurant base CRUD files

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/js/all.min.js';
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
--- src/Controller/Admin/RestaurantController.php
+++ src/Controller/Admin/RestaurantController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Restaurant;
 use App\Form\RestaurantType;
